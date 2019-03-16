34e0b5821b68263d879d0713084d73abb69ec69c
Michael Kuryshev <me@mk9.name>
2019-03-16 12:25:03 +0100

    switched to google-closure-compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const ClosurePlugin = require("closure-webpack-plugin");
 
 module.exports = {
   entry: {
     lib: "./src/lib.ts",
     entrypoint: "./src/entrypoint.ts"
+  },
+  optimization: {
+    minimizer: [
+      new ClosurePlugin(
+        { mode: "STANDARD" },
+        {
+          compilation_level: "ADVANCED",
+          externs: "externs.js"
+        }
+      )
+    ]
   },
   module: {
     rules: [
